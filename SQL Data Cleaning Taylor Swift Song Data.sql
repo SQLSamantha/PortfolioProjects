5cf534063fc7f6dfb8bf82b547eba7c68e68d9c5
Samantha McNulty <147660706+SQLSamantha@users.noreply.github.com>
2023-10-23 12:37:24 -0400

    SQL Data Cleaning Taylor Swift Song Data.sql

--- SQL Data Cleaning Taylor Swift Song Data.sql
+++ SQL Data Cleaning Taylor Swift Song Data.sql
@@ -10,21 +10,21 @@
 
 --Taylor_Swift_Spotify:
 
--- ID - simplified unique ID for each song
+-- ID - a simplified unique ID for each song
 -- Song - the name of the song
 -- Album - the name of the album
--- Release_Date - the day month and year the album was released
+-- Release_Date - YYYY -MM-DD - the date the album was released
 -- Track_Number - the order the song appears on the album
 -- Spotify_ID - the Spotify id for the song
 -- Spotify_ID - the Spotify uri for the song
--- Acousticness - A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic.
--- Danceability - Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable.
--- Energy - Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy.
--- Instrumentalness - Predicts whether a track contains no vocals. "Ooh" and "aah" sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly "vocal". The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0.
--- Liveness - Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live.
--- AvgDecibels - The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks. Loudness is the quality of a sound that is the primary psychological correlate of physical strength (amplitude). Values typically range between -60 and 0 db.
--- Speechiness - Detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value. Values above 0.66 describe tracks that are probably made entirely of spoken words. Values between 0.33 and 0.66 describe tracks that may contain both music and speech, either in sections or layered, including such cases as rap music. Values below 0.33 most likely represent music and other non-speech-like tracks.
--- Tempo - The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration.
+-- Acousticness - 0.0 - 1.0 - whether the track is acoustic. 1.0 represents high confidence the track is acoustic.
+-- Danceability - 0.0 - 1.0 - how suitable the song is for dancing
+-- Energy - 0.0 - 1.0 - how energetic the song is
+-- Instrumentalness - 0.0 - 1.0 - The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content.
+-- Liveness - 0.0 - 1.0 - Detects the presence of an audience in the recording.
+-- AvgDecibels - The overall loudness of a track in decibels (dB). 
+-- Speechiness - 0.0 - 1.0 - Detects the presence of spoken words in a track. The closer to 1.0 the attribute value. Values above 0.66 describe tracks that are probably made entirely of spoken words. Values between 0.33 and 0.66 describe tracks that may contain both music and speech. Values below 0.33 most likely represent music.
+-- Tempo - The overall estimated tempo of a track in beats per minute (BPM). 
 -- Valence - A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).
 -- CurrentPopularity - the popularity of the song from 0 to 100. This is straight from the Spotify API and is a 0-100 range based on how 'hot' the track is across the platform.
 -- Duration - The duration of the track in milliseconds.
@@ -324,13 +324,13 @@ FROM Taylor_Swift_Spotify
 WHERE SongTitle LIKE '%Not Sorry%'
 
 --You're Not Sorry
---You’re Not Sorry
+--YouÂ’re Not Sorry
 
 -- There is also a song called "Back To December/Apologize/You're Not Sorry", so I needed to make sure to exclude that from my changes
 
 SELECT SongTitle
 FROM Taylor_Swift_Spotify
-WHERE SongTitle LIKE '%You’re Not Sorry%'
+WHERE SongTitle LIKE '%YouÂ’re Not Sorry%'
 
 SELECT SongTitle
 FROM Taylor_Swift_Spotify
@@ -338,7 +338,7 @@ WHERE SongTitle LIKE '%Not Sorry%'
 AND SongTitle NOT LIKE '%Back To December/Apologize%'
 
 UPDATE Taylor_Swift_Spotify 
-SET SongTitle = 'You’re Not Sorry'
+SET SongTitle = 'YouÂ’re Not Sorry'
 WHERE SongTitle LIKE '%Not Sorry%' 
 AND SongTitle NOT LIKE '%Back To December/Apologize%'
 
